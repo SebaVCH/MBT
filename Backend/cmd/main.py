4b5e825fc907782283b97f9cfbda32776d8fc4bd
SebaVCH <sebastianvchepillo@gmail.com>
2025-09-13 17:11:55 -0300

    Feat: New welcome chatbot
    - A welcome message was added when the application starts

--- Backend/cmd/main.py
+++ Backend/cmd/main.py
@@ -1,3 +1,6 @@
 from cmd.app.setup import StartBackend
+from internal.utils.aiBot import welcome
+
 # Iniciar backend
+welcome()
 app = StartBackend()
---
+++ Backend/internal/utils/aiBot.py
@@ -0,0 +1,7 @@
+import os
+
+import ollama
+
+welcomeMessage = ollama.generate(model = os.getenv("AI_MODEL"), prompt = "Generame un saludo de bienvenida")
+def welcome():
+    print(welcomeMessage)
